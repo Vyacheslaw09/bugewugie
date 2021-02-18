84976a5ca829011e5144cc60afd21687ef0d6d9d
DBalakishyieu <dima.balakishiev.99@mail.ru>
2021-02-18 21:32:46 +0300

    implement js for 3 task

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,32 @@
-console.log('Hi from js');
+// Массив наших дел
+var todos = []
+function click() {
+    console.log('some')
+}
+/*
+    Функция  которяа добавляет элемент к списку дел
+*/
+function newElement () {
+    // значение поля ввода, дело которое мы добавляем
+    var input = document.getElementById("myInput").value;
+    // добавляем наше дело в общий список (понадобиться для будующего)
+    todos.push(input);
+    // Выводим наш список в консоль (просто для проверки, консоль можно открыть нажав F12)
+    console.log(todos);
+    // элемент ul - где обертка списка
+    var ul = document.getElementById("myUL");
+    // создаем новый элемент для списка
+    var li = document.createElement("li");
+    // Добавляем текст нашему элементу списка li
+    li.appendChild(document. createTextNode(input));
+    // пример для дополнительного задания
+    // li.onclick = click;
+    //
+    // Добавляем дело к нашему списку
+    ul.appendChild(li);
+}
+
+// function click() {
+//     alert('click')
+// }
+
